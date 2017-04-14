3379ef1a9a1fe2e543a452a183940177ca774dd2
Rafael <rafaueeda@gmail.com>
2017-04-14 15:52:22 -0300

    Faltam apenas wait e notify

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <bool.h>
+#include <stdbool.h>
 #include <unistd.h>
+#include <time.h>
 
 ////////////////////////////// Variáveis Globais
 
@@ -10,21 +11,28 @@ int caldeirao = 0;
 int iniciaThread;
 pthread_t selvagens[30];
 pthread_t cozinheiro;
-pthread_mutex_t mutex1 = pthread_mutex_init (&mutex1, NULL );;
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 
 ////////////////////////////// Structs
 
+struct structCanibal{
+    int qntComeu;
+};
+
+struct structCozinheiro{
+    int qtdCozinhou;
+};
+
 ////////////////////////////// Métodos
 
-void adicionarCaldeirao();
-void consumirCaldeirao();
+void *adicionarCaldeirao(void *ptr);
+void comer();
+void *servir(void *ptr);
 
 ////////////////////////////// Criação da Thread
 
-// iret1 = pthread_create( &thread(i), NULL, comer, (void*)  i);
-
 int pthread_create(pthread_t *thread, //identificador)
-	pthread_attr_t *attr, //mudar estado da thread)
+    const pthread_attr_t *attr, //mudar estado da thread)
 	void *(*start_routine)(void *), //funcao que ira rodar na thread)
 	void *arg); //argumento para a funcao)
 
@@ -36,28 +44,38 @@ int pthread_join(pthread_t th, //ID da thread)
 
 int main()
 {
+    int cont = 0;
     for(int i =0; i<30; i++){
-        iniciaThread = pthread_create( &consumidor[i], NULL, consumirCaldeirao, (void*)  i);
+        iniciaThread = pthread_create( &selvagens[i], NULL, (void*)&servir, (void*)i);
     }
 
-    iniciaThread = pthread_create( &cozinheiro, NULL, adicionarCaldeirao, (void*) cozinheiro);
-
-    printf("Hello Word");
+    iniciaThread = pthread_create( &cozinheiro, NULL, (void*)&adicionarCaldeirao, (void*)cozinheiro);
+    cont++;
     return 0;
 }
 
-void *adicionarCaldeirao(void*ptr){
+void *adicionarCaldeirao(void *ptr){
+    pthread_mutex_lock(&mutex1);
     caldeirao = 12;
     sleep(7000);
+    pthread_mutex_unlock(&mutex1);
 }
 
-void *consumirCaldeirao(void*ptr){
+void *servir(void *ptr){
+    printf("comendo ");
     if(caldeirao > 0){
         pthread_mutex_lock(&mutex1);
         caldeirao--;
         pthread_mutex_unlock(&mutex1);
+        comer();
     }
     else{
-
+        *adicionarCaldeirao(&cozinheiro);
     }
 }
+
+void comer(){
+    int dormir = rand() % 3;
+    sleep(dormir + 1);
+}
+
